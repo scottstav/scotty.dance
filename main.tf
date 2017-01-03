e379a9fc8db926344174943152bbcc2cdfa9bde2
Sam Stavinoha <smlstvnh@gmail.com>
2017-01-02 22:33:13 -0600

    add records for hugo site

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ module "mailer" {
   domain                = "${var.domain}"
   mailgun_smtp_password = "${var.mailgun_smtp_password}"
 }
+
+resource "aws_route53_record" "root" {
+   zone_id = "${module.mailer.zone_id}"
+   name = "${var.domain}"
+   type = "A"
+   ttl = "300"
+   records = ["s3-website-us-east-1.amazonaws.com."]
+}
+
+resource "aws_route53_record" "www" {
+   zone_id = "${module.mailer.zone_id}"
+   name = "www.${var.domain}"
+   type = "A"
+   ttl = "300"
+   records = ["s3-website-us-east-1.amazonaws.com."]
+}
