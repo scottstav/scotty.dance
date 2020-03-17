ab531b4f5902a3917fbdbc8cf8e5b1f582dc43eb
scottstav <howdy@scotty.dance>
2020-03-17 03:06:34 -0700

    serverless blog

--- sam-ruby/src/app.rb
+++ sam-ruby/src/app.rb
@@ -1,48 +1,61 @@
-# require 'httparty'
+# coding: utf-8
 require 'json'
+require_relative 'post_dao'
+require_relative 's3_helper'
 
-def get_prompt(event:, context:)
+class Blog
 
-  {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-   },
-    body: {
-      prompt: "How did i get here?",
-      # location: response.body
-    }.to_json
-  }
-end
+  def initialize
+    @post_dao = PostDAO.new
+    @s3_helper = S3Helper.new
+  end
 
-def put_answer(event:, context:)
+  # Add new entry to dynamo
+  # Create S3 object
+  # Return pre-signed S3 URL for PUT
+  # If title already exists, return a fresh signed URL
+  def create_post(event:, context:)
+    post_title = event["body"]["title"]
+    return lambda_response(statusCode: 400, body: {message: 'Unable to create post. Check your request'}) if !post_title
+    begin
+      id = @post_dao.create_post(title: post_title)
+      url = @s3_helper.signed_put_url(key: id)
+    rescue StandardError => error
+      puts "create_post failed with the following error: #{error.message}"
+      return lambda_response(statusCode: 500, body: {message: error.message})
+    end
 
-  {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-   },
-    body: {
-      message: "Everything is fine!",
-      # location: response.body
-    }.to_json
-  }
-end
+    lambda_response(
+      statusCode: 200,
+      body: { request_url: url }
+    )
+  end
 
-def get_answers(event:, context:)
+  def get_posts(event:, context:)
+    ddb_posts = @post_dao.get_posts
+    begin
+      ddb_posts.each do |p|
+        post_markdown = @s3_helper.get_object_content(name: p['id'])
+        byebug
+        p[:markdown] = post_markdown
+      end
+    rescue StandardError => error
+      puts "get_posts failed with the following error: #{error.message}"
+      return lambda_response(statusCode: 500, body: {message: error.message})
+    end
 
-  {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-   },
-    body: {
-      answers: [
-      ],
-      # location: response.body
-    }.to_json
-  }
+    lambda_response(
+      statusCode: 200,
+      body: { posts: ddb_posts }
+    )
+  end
+
+  def lambda_response(statusCode:, body:)
+    {
+      statusCode: statusCode,
+      headers: {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"},
+      body: body.to_json
+    }
+  end
+  
 end
