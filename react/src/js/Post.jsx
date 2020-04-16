ffdafe9801a2f6766b7a4d5d0a7ab61853f3a170
scottstav <howdy@scotty.dance>
2020-04-15 19:22:01 -0700

    Video support

--- react/src/js/Post.jsx
+++ react/src/js/Post.jsx
@@ -5,6 +5,15 @@ import ReactMarkdown from "react-markdown";
 
 class Post extends Component {
 
+  getFileExtension(s) {
+    let patt1 = /\.([0-9a-z]+)(?:[\?#]|$)/i;
+    let fileExtensions = [];
+    if (s && s.match(patt1)) {
+      fileExtensions = s.match(patt1);
+    }
+    return fileExtensions;
+  }
+
   render() {
     let postId = this.props.postId ? this.props.postId : this.props.match.params.postId;
     let post = this.props.posts.filter(function(p) { return p.object_key == postId; })[0];
@@ -17,11 +26,7 @@ class Post extends Component {
 	);
       },
       link: (props) => {
-	let patt1 = /\.([0-9a-z]+)(?:[\?#]|$)/i;
-	let fileExtensions = [];
-	if (props.href && props.href.match(patt1)) {
-	  fileExtensions = props.href.match(patt1);
-	}
+	let fileExtensions = this.getFileExtension(props.href);
 	if (fileExtensions.includes("ogg") || fileExtensions.includes("m4a")) {
 	  return(
 	    <span className="audio-player">
@@ -39,6 +44,25 @@ class Post extends Component {
 	} else {
 	  return (
 	    <a href={props.href}>{props.children[0].props.value}</a>
+	  );
+	}
+      },
+      image: (props) => {
+	let fileExtensions = this.getFileExtension(props.src);
+	if (fileExtensions.includes("mov") || fileExtensions.includes("mp4")) {
+	  return(
+	    <video
+	      muted
+	      controls
+	      src={props.src}>
+	      Your browser does not support the
+	      <code>video</code> element. So you are missing out on
+	      <b>{props.alt}</b>
+	    </video>
+	  );
+	} else {
+	  return (
+	    <img src={props.src} alt={props.alt}></img>
 	  );
 	}
       }
