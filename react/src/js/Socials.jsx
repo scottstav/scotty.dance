01b00288269a5178c241e4e23056815ce3b4913d
scottstav <howdy@scotty.dance>
2020-08-09 20:28:41 -0500

    --

--- react/src/js/Socials.jsx
+++ react/src/js/Socials.jsx
@@ -18,9 +18,7 @@ class Socials extends Component {
       <div id="socials">
 	<div><a href="mailto:howdy@scotty.dance"><i className="fas fa-envelope"></i></a></div>
 	<div><a href="https://github.com/scottstav"><i className="fab fa-github"></i></a></div>
-	<div><a href="https://twitter.com/awkwardnessful"><i className="fab fa-twitter"></i></a></div>
 	<div><a href="https://keybase.io/scottstav"><i className="fab fa-keybase"></i></a></div>
-	<div><a href="https://www.linkedin.com/in/scottstav/"><i className="fab fa-linkedin"></i></a></div>
 	<div><a href="https://untappd.com/user/scottstav"><i className="fab fa-untappd"></i></a></div>
 	<div>{iMessageLink}</div>
       </div>
