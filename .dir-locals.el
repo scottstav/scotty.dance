3d552855fb57fee230e376c222754a09ef2168c4
scottstav <scotts@ifit.com>
2020-12-01 23:05:08 -0600

    cute 💐

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(
- (nil .
-      (
-       (eval add-hook 'after-save-hook '(lambda () (org-publish-project "org"))  nil t)))
- )
+;; (
+;;  (nil .
+;;       (
+;;        (eval add-hook 'after-save-hook '(lambda () (org-publish-project "org"))  nil t)))
+;;  )
