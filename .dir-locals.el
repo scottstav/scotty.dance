c61a17f5337e63689445051bb095a72cb728eadd
scottstav <howdy@scotty.dance>
2021-05-25 09:18:00 -0500

    rm dir-locals.el

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-;; (
-;;  (nil .
-;;       (
-;;        (eval add-hook 'after-save-hook '(lambda () (org-publish-project "org"))  nil t)))
-;;  )
